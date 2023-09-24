a3f19577aea0df0452c75bbb9ff4fbe45c610278
djbefolo <121225567+djbefolo@users.noreply.github.com>
2023-09-24 12:23:43 +0200

    source code

--- models_web_api.py
+++ models_web_api.py
@@ -1,4 +1,5 @@
 # Dependencies
+import os
 from os import system
 from flask import Flask, request, jsonify
 
@@ -73,7 +74,8 @@ def predictByClientId():
 
 if __name__ == '__main__':
     try:
-        port = int(system.argv[1]) # This is for a command-line input
+        port = int(os.environ.get('PORT', 5000))
+      
     except:
         port = 12345 # If you don't provide any port the port will be set to 12345
 
